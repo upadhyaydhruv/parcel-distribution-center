ec508b525fc77c2518d5048f1f3f1263f3b9b1c0
Dhruv Upadhyay <dmupadhyay12@gmail.com>
2020-04-14 01:31:32 -0400

    Update Parcel.java
    Fixed encapsulation errors and improved code quality

--- Parcel.java
+++ Parcel.java
@@ -6,6 +6,8 @@ public class Parcel {
     private int x, y;
     private int xVel = 1;
     private int yVel = 0;
+    private int counter = 0;
+
     public Parcel(int l, int w, int h, int color, int startX, int startY){
        this.l = l;
        this.w = w;
@@ -13,6 +15,25 @@ public class Parcel {
        this.color = color;
        this.x = startX;
        this.y = startY;
+    }
+
+    public int getX(){
+        return this.x;
+    }
+    public int getY(){
+        return this.y;
+    }
+
+    public void setXVel(int xVel){
+        this.xVel = xVel;
+    }
+
+    public void setYVel(int yVel){
+        this.yVel = yVel;
+    }
+
+    public int getColor(){
+        return this.color;
     }
 
     public void move(){
@@ -30,19 +51,20 @@ public class Parcel {
         else if (color==2){
             g.setColor(Color.YELLOW);
         }
-        int[] xPoints = {x, x, x+l, (int)(x+l+Math.cos(45)*w), (int)(x+l+Math.cos(45)*w), (int)(x+l+Math.cos(45)*w-l), x};
-        int[] yPoints = {y, y-h, y-h, (int)(y-h+Math.cos(45)*w), (int)(y-h+Math.cos(45)*w+h), (int)(y-h+Math.cos(45)*w+h), y};
+
+        int[] xPoints = {x, (int)(x-w*Math.cos(45)), (int)(x-w*Math.cos(45)), (int)(x-w*Math.cos(45)+l), x+l, x+l, x};
+        int[] yPoints = {y, (int)(y-w*Math.cos(45)), (int) (y-w*Math.cos(45)-h), (int) (y-w*Math.cos(45)-h), y-h, y, y};
 
         g.fillPolygon(xPoints, yPoints, 6);
         g.setColor(Color.BLACK);
-        g.drawLine((int)(x+l+Math.cos(45)*w-l),(int)(y-h+Math.cos(45)*w+h), (int)(x+l+Math.cos(45)*w-l), (int)(y-h+Math.cos(45)*w));
-        g.drawLine((int)(x+l+Math.cos(45)*w-l), (int)(y-h+Math.cos(45)*w),x, y-h);
-        g.drawLine((int)(x+l+Math.cos(45)*w-l), (int)(y-h+Math.cos(45)*w), (int)(x+l+Math.cos(45)*w), (int)(y-h+Math.cos(45)*w));
-        g.drawLine(x,y, x, y-h);
-        g.drawLine(x,y-h,x+l,y-h);
-        g.drawLine(x+l,y-h,(int)(x+l+Math.cos(45)*w),(int)(y-h+Math.cos(45)*w));
-        g.drawLine((int)(x+l+Math.cos(45)*w),(int)(y-h+Math.cos(45)*w),(int)(x+l+Math.cos(45)*w),(int)(y-h+Math.cos(45)*w+h));
-        g.drawLine((int)(x+l+Math.cos(45)*w),(int)(y-h+Math.cos(45)*w+h),(int)(x+l+Math.cos(45)*w-l),(int)(y-h+Math.cos(45)*w+h));
-        g.drawLine((int)(x+l+Math.cos(45)*w-l),(int)(y-h+Math.cos(45)*w+h),x,y);
+        g.drawLine(x, y, x+l, y);
+        g.drawLine(x+l, y, x+l, y-h);
+        g.drawLine(x+l, y-h, (int) (x+l-w*Math.cos(45)), (int)(y-h-w*Math.cos(45)));
+        g.drawLine((int) (x+l-w*Math.cos(45)),(int)(y-h-w*Math.cos(45)), (int) (x+l-w*Math.cos(45))-l, (int)(y-h-w*Math.cos(45)));
+        g.drawLine((int) (x+l-w*Math.cos(45))-l, (int)(y-h-w*Math.cos(45)),(int) (x+l-w*Math.cos(45))-l,(int)(y-w*Math.cos(45)));
+        g.drawLine((int) (x+l-w*Math.cos(45))-l,(int)(y-w*Math.cos(45)),x, y);
+        g.drawLine(x, y, x, y-h);
+        g.drawLine(x, y-h, (int) (x-w*Math.cos(45)),(int) (y-h-w*Math.cos(45)));
+        g.drawLine(x, y-h, x+l, y-h);
     }
 }
