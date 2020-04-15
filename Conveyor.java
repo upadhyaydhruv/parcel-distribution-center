07b5feda75210367c0e93a8773ec851ce54678aa
Dhruv Upadhyay <dmupadhyay12@gmail.com>
2020-04-15 00:16:26 -0400

    Update Conveyor.java
    Added functionality for multi-axis conveyor belts

--- Conveyor.java
+++ Conveyor.java
@@ -2,20 +2,13 @@ import java.awt.*;
 
 public class Conveyor {
     private int direction;
-    private int xVel;
-    private int yVel;
-    private boolean on;
+    private int vel;
+    private boolean on = false;
     private int y;
     private int x;
     private int offset = 0;
     private int width, length;
-    private int lineX, lineY;
     private boolean paused = false;
-    private boolean twoInUse = false;
-    private boolean threeInUse = false;
-    private boolean fourInUse = false;
-
-
 
     public Conveyor(int direction, int x, int y, int width, int length) {
         this.direction = direction;
@@ -23,46 +16,46 @@ public class Conveyor {
         this.y = y;
         this.width = width;
         this.length = length;
-        this.lineX = x;
-        this.lineY = y;
-
-        //if (direction==1 || direction==3) {
-        //    this.xVel = 1;
-        //    this.yVel = 0;
-        //}
-        //else {
-        //    this.xVel = 0;
-        //    if (direction == 2) { // upwards conveyor
-        //        this.yVel = -1;
-        //    }
-        //    if (direction == 4) {
-        //        this.yVel = 1;
-        //    }
-        //}
     }
 
-    public void move(){
-        xVel = 1;
-        yVel = 0;
-        if (!paused && direction==1 || (twoInUse&&direction==2)){
-            lineX+=xVel;
-            if (lineX>70){
-                lineX = 0;
+    public void move(Parcel[] p){
+        this.on = false;
+        this.vel = 1;
+        for (Parcel parcel:p){
+            if (parcel.getX()>this.x&&parcel.getX()<this.x+width&&parcel.getY()>this.y&&parcel.getY()<this.y+this.length){
+                this.on = true;
             }
-            lineY+=yVel;
         }
-
-        else if (!twoInUse&&direction==2){
-            xVel = 0;
+        if (on&&!this.isPaused()){
+            offset+=vel;
+            if (offset>70){
+                offset = 0;
+            }
+            }
+        else {
+            vel = 0;
         }
     }
+
 
     public void paint(Graphics2D g){
         g.setColor(Color.DARK_GRAY);
         g.fillRect(x, y, width, length);
         g.setColor(Color.WHITE);
-        for (int i=x+lineX; i<x+width; i+=70){
-            g.drawLine(i, y, i, y+length);
+        if (direction==1 || direction == 3) {
+            for (int i = x + offset; i < x + width; i += 70) {
+                g.drawLine(i, y, i, y + length);
+            }
+        }
+        else if (direction==2){
+            for (int i=y + length -  offset; i > y; i-=70){
+                g.drawLine(x, i, x+width, i);
+            }
+        }
+        else if (direction==4){
+            for (int i = y+offset; i<y+length; i+=70){
+                g.drawLine(x, i, x+width, i);
+            }
         }
     }
 
@@ -70,17 +63,6 @@ public class Conveyor {
         return paused;
     }
 
-    public void getParcelState(Scanner s){
-        if (s.getScanned() && s.getColor()==0){
-            twoInUse = true;
-        }
-        else if (s.getScanned() && s.getColor()==1){
-            threeInUse = true;
-        }
-        else if (s.getScanned() && s.getColor()==2){
-            fourInUse = true;
-        }
-    }
 
     public void setPaused(Scanner s) {
         this.paused = s.isRunning();
